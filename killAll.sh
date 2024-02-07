2b3219a1e541ba6852566b13b884ba1eef5d87b6
Ecla1r <ecla111r@gmail.com>
2024-02-07 20:33:02 +0300

    upd

--- killAll.sh
+++ killAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for proc in $(ps aux|grep "node ./src/main.js"|awk '{print $2}'); do kill $proc; done
+for proc in $(ps aux|grep "node ./src/main.js"|awk '{print $2}'); do pkill $proc; done
 
 
